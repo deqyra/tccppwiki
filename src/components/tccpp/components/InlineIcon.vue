906e5cb6b457cc21308543d492caf87fd295cb4b
François Brachais <francois.brachais@free.fr>
2025-03-24 03:19:56 +0100

    Add InlineIcon vue component

---
+++ src/components/tccpp/components/InlineIcon.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{
+    src: string;
+    alt?: string;
+}>();
+</script>
+
+<style module>
+.inline_icon {
+    width: 14pt;
+    display: inline;
+}
+</style>
+
+<template>
+    <img v-bind="props" :class="$style.inline_icon" />
+</template>
